c422fffa68f2e54e53a68e669a1428ba2f1f6d20
Abdud Dayyaan Howell <abhowcpt025@student.wethinkcode.co.za>
2026-07-27 19:36:15 +0200

    Feat : Added Opperating system scan

--- Device_Scanner.py
+++ Device_Scanner.py
@@ -4,6 +4,7 @@
 
 import socket
 import uuid
+import platform
 
 print("\n======================")
 print("=== Device Scanner ===")
@@ -26,5 +27,7 @@ mac_address = ':'.join(f"{(mac >> ele) & 0xff:02X}" for ele in range(40, -1, -8)
 
 # _________________________________________________________________________________________________
 
+operating_system = f"{platform.system()} {platform.release()}"
 
+# _________________________________________________________________________________________________
 
